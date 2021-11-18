ec605243c29c5272d8a7a654c33d10e12a3cc5bb
Pablo Villarroel <pablovillarroel135@gmail.com>
2021-11-18 17:15:11 -0300

    added cors

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+app.options('*', cors())
 
 // Rutas
 const marcaRoute = require('./routes/marcaRoute')
